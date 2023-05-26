954dcca0e6835ce377dc46828acf75d50b04153d
Johan-Leandro-S <johanjls2003@gmail.com>
2023-05-26 13:42:42 -0500

    johan

--- js/citas.js
+++ js/citas.js
@@ -123,3 +123,54 @@ function buscar(idPaciente) {
         }
     });
 } */
+
+function changeForm() {
+  var sedesSelect = document.getElementById("sedes");
+  var especialidadForm = document.getElementById("especialidadForm");
+
+  if (sedesSelect.value === "San Miguel") {
+    especialidadForm.innerHTML = `
+    <label for="lang" class="font-weight-bold">Especialidad</label><br>
+    <select name="Especialidad" id="lang" class="border border-dark">
+      <option value="CIRUGÍA GENERAL" class="border border-dark">CIRUGÍA GENERAL</option>
+      <option value="DENSITOMETRÍA" class="border border-dark">DENSITOMETRÍA</option>
+      <option value="DERMATOLOGÍA" class="border border-dark">DERMATOLOGÍA</option>
+      <option value="ECOGRAFÍA" class="border border-dark">ECOGRAFÍA</option>
+      <option value="GASTROENTEROLOGÍA" class="border border-dark">GASTROENTEROLOGÍA</option>
+      <option value="GERIATRÍA" class="border border-dark">GERIATRÍA</option>
+      <option value="GINECOLOGÍA" class="border border-dark">GINECOLOGÍA</option>
+      <option value="LABORATORIO" class="border border-dark">LABORATORIO</option>
+      <option value="MEDICINA ESTÉTICA" class="border border-dark">MEDICINA ESTÉTICA</option>
+      <option value="MEDICINA GENERAL " class="border border-dark">MEDICINA GENERAL </option>
+    </select>`;
+  } else if (sedesSelect.value === "Lince") {
+    especialidadForm.innerHTML = `
+      <label for="lang" class="font-weight-bold">Especialidad</label><br>
+      <select name="Especialidad" id="lang" class="border border-dark">
+        <option value="DENSITOMETRÍA" class="border border-dark">DENSITOMETRÍA</option>
+        <option value="GASTROENTEROLOGÍA" class="border border-dark">GASTROENTEROLOGÍA</option>
+        <option value="LABORATORIO" class="border border-dark">LABORATORIO</option>
+      </select>`;
+    } else if (sedesSelect.value === "La Molina") {
+        especialidadForm.innerHTML = `
+          <label for="lang" class="font-weight-bold">Especialidad</label><br>    
+          <select name="Especialidad" id="lang" class="border border-dark">
+            <option value="LABORATORIO" class="border border-dark">LABORATORIO</option>
+            <option value="NEUROLOGÍA" class="border border-dark">NEUROLOGÍA</option>
+            <option value="NUTRICIÓN" class="border border-dark">NUTRICIÓN</option>
+          </select>`;
+  } else if (sedesSelect.value === "Surco") {
+    especialidadForm.innerHTML = `
+      <label for="lang" class="font-weight-bold">Especialidad</label><br>    
+      <select name="Especialidad" id="lang" class="border border-dark">
+        <option value="PSICOLOGÍA" class="border border-dark">PSICOLOGÍA</option>
+        <option value="CARDIOLOGÍA" class="border border-dark">CARDIOLOGÍA</option>
+        <option value="REUMATOLOGÍA" class="border border-dark">REUMATOLOGÍA</option>
+      </select>`;
+} else {
+    especialidadForm.innerHTML = `
+      <label for="lang" class="font-weight-bold">Escoga una Sede</label><br>
+      <select name="Especialidad" id="lang" class="border border-dark">
+      </select>`;
+  }
+}
